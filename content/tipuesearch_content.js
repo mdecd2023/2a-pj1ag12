fdef44591fb700cd97dbfc7427736f2b7ca30b72
41023126 <41023126@gm.nfu.edu.tw>
2023-03-07 16:50:04 +0800

    Revise content

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'members: \n 41023114 王樟皓 41023126 卓桓琮 \n 倉儲: \xa0 https://github.com/mdecd2023/2a-pj1ag12 \n 網站: \xa0 https://mdecd2023.github.io/2a-pj1ag12', 'tags': '', 'url': 'About.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲: https://github.com/mdecd2023/2a-pj1ag12.git', 'tags': '', 'url': 'About.html'}]};
->>>>>>> 87a09fe52aa603659232e064f6607e3ec918e05f
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'members: \n 41023114 王樟皓 41023126 卓桓琮 \n 倉儲: \xa0 https://github.com/mdecd2023/2a-pj1ag12 \n 網站: \xa0 https://mdecd2023.github.io/2a-pj1ag12 \n', 'tags': '', 'url': 'About.html'}]};
