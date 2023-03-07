47e081601414026af2a601b875645050359cd857
41023126 <41023126@gm.nfu.edu.tw>
2023-03-07 16:40:35 +0800

    add new contect

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'members: \n 41023114 王樟皓 41023126 卓桓琮 \n 倉儲: \xa0 https://github.com/mdecd2023/2a-pj1ag12 \n 網站: \xa0 https://mdecd2023.github.io/2a-pj1ag12', 'tags': '', 'url': 'About.html'}]};
