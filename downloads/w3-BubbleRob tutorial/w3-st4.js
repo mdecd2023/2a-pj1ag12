46be2fd66181a79df945f37d6733c44a0f29c9f0
41023126 <41023126@gm.nfu.edu.tw>
2023-03-10 15:00:17 +0800

    add w3-bubbleRob-stage4-5-6

---
+++ downloads/w3-BubbleRob tutorial/w3-st4.js
@@ -0,0 +1,11 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "280": [
+      { type: "gotoframe", rect: { x: 334, y: 63, width: 60, height: 24 }, target: 0 },
+    ],
+  },
+};
